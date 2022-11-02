f44a2dd693e04bc25c81b9b42e34ece4015f9b8f
Pavel <rut100500@yandex.ru>
2022-11-02 18:56:01 +0300

    Задача с bool

---
+++ var1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int enter = Convert.ToInt32(Console.ReadLine()); 
+int thriedDig = GetThirdNum(enter);
+Console.WriteLine(thriedDig);
+
+int GetThirdNum(int num)
+{
+    int result = num % 100 / 10;
+    bool res = CheckLenNum(num);
+    return result;
+}
+
+bool CheckLenNum(int number)
+{
+    return number < 1000 && number > 99;
+}
+
