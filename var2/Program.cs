fa57303d89b127894c7b8f00a5c41009a911acce
Pavel <rut100500@yandex.ru>
2022-11-04 09:06:05 +0300

    Victory

---
+++ var2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int input = Convert.ToInt32(Console.ReadLine());
+bool result = CheckNum(input);
+if (result == false)
+{
+    Console.WriteLine("Число не трехзначное");
+}
+else
+{
+    Console.WriteLine("Ураааа!! Победа");
+}
+
+
+bool CheckNum(int num)
+{
+    return num < 1000 && num > 99;
+}
