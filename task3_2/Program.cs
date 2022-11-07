063cbc4f70f8d94b1f104d6722337227d7d9c84d
Pavel <rut100500@yandex.ru>
2022-11-07 07:53:55 +0300

    Расстояние по координатам

---
+++ task3_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Получить две точки и найти расстояние между ними в 2D
+Console.WriteLine("Введите координаты первой точки: ");
+bool isPointAX = int.TryParse(Console.ReadLine(), out int ax);
+bool isPointAY = int.TryParse(Console.ReadLine(), out int ay);
+
+Console.WriteLine("Введите координаты второй точки: ");
+bool isPointBX = int.TryParse(Console.ReadLine(), out int bx);
+bool isPointBY = int.TryParse(Console.ReadLine(), out int by);
+
+if(!isPointAX || !isPointAY || !isPointBX || !isPointBY)
+{
+    Console.WriteLine("Ошибка! Ввели не то. переВВедите.");
+    return;
+}
+
+double Res = Distance(ax, ay, bx, by);
+Console.WriteLine($"Расстояние между точками = {Res}");
+
+double Distance(int x1, int y1, int x2, int y2)
+{
+    double dist = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    return dist;
+}
---
+++ var3/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введите число в котором не менее трех знаков");
+int enter = Convert.ToInt32(Console.ReadLine());
+if(enter <100)
+{
+    Console.WriteLine("Ошибка! В этом числе меньше трех знаков!");
+}
+else
+    {
+    int third = GetThirdNum(enter);
+    Console.WriteLine(third);
+    }
+
+
+int GetLengtNum(int num)
+{
+    int count = 0;
+    while(num > 0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+
+}
+
+int GetThirdNum(int input)
+{
+    int lenght = GetLengtNum(input);
+    int[] array = new int[lenght];
+
+    for (int i = 0; i < lenght; i++)
+    {
+        array[i] = input % 10;
+        input /=10;
+    }
+    int res = (array[lenght-3]);
+    return res;
+}
+
